d97c0efdce19f7679b822dc957a3200adc8baf2a
Dante Nardo <nardogamedev@outlook.com>
2025-02-19 17:58:38 -0500

    feat: Web Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,14 @@ import { remarkReadingTime } from "./plugins/remark-reading-plugin.mjs";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
 	site: "https://thedolentchronicle.com",
+	output: "static",
+	adapter: vercel(),
 	integrations: [react()],
 	markdown: {
 		remarkPlugins: [remarkReadingTime],

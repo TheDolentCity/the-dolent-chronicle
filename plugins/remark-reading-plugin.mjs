d5cf0cc9adbba6dd41ebe48b6cd097fe6f516e2a
DanteNardo <nardogamedev@outlook.com>
2025-12-06 17:06:33 -0500

    feat: Cleanup

--- plugins/remark-reading-plugin.mjs
+++ plugins/remark-reading-plugin.mjs
@@ -1,14 +1,6 @@
 import getReadingTime from "reading-time";
 import { toString } from "mdast-util-to-string";
 
-// export function remarkReadingTime() {
-//   return function (tree, { data }) {
-//     const text = toString(tree);
-//     const readingTime = getReadingTime(text);
-//     data.astro.frontmatter.minutes = Math.round(readingTime.minutes);
-//   };
-// }
-
 export function remarkReadingTime() {
 	return function (tree, { data }) {
 		// Create a deep clone

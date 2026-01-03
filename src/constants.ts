7194bb23465968784229b7c39cbb824fe8e31779
Dante Nardo <nardogamedev@outlook.com>
2026-01-03 08:22:39 -0500

    feat: Added favicons

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const SITE_TITLE = 'The Dolent Chronicle';
-export const SITE_DESCRIPTION = "A paper printed in the stone halls of the dead. An RPG blog for lonely spiders, inky funghi, and those who 'Play Worlds, Not Rules'.";
+export const SITE_DESCRIPTION = "A paper printed in the stone halls of the dead. An RPG blog for lonely spiders, inky funghi, and those who \"Play Worlds, Not Rules\".";

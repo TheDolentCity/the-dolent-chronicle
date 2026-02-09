8ce7c5cf93af8924179973693680038aa3f78080
Dante Nardo <nardogamedev@outlook.com>
2026-02-08 21:40:30 -0500

    feat: Removed beanie

--- src/components/app/PageNewsletterButton.tsx
+++ src/components/app/PageNewsletterButton.tsx
@@ -7,10 +7,6 @@ export function PageNewsletterButton() {
 			title="Sign up to newsletter"
 			className="not-prose absolute -left-10 lg:-left-14 top-1 lg:top-2 p-2 rounded-full text-zinc-600 dark:text-zinc-400 hover:bg-white hover:dark:bg-zinc-800 hover:text-zinc-900 dark:hover:text-zinc-100 mst"
 		>
-			{/* <p className="hidden sm:block absolute -left-30 -top-20 font-beanie text-3xl text-amber-700 dark:text-amber-400">
-				Newsletter
-				<br /> signup!
-			</p> */}
 			<Newspaper className="size-4 lg:size-6" />
 		</a>
 	);
